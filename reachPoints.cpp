f4c61d624701a7998f7d824e45e90bad18f1a431
Noopur R K <34936362+noopurrkalawatia@users.noreply.github.com>
2019-09-23 23:17:10 -0400

    Create reachPoints.cpp
    Solution for : https://leetcode.com/articles/reaching-points/

---
+++ reachPoints.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) 
+    {
+        while(tx >= sx && ty >= sy)
+        {
+            if(tx == ty)
+            {
+                break;
+            }
+            if(tx > ty)
+            {
+                if(ty > sy)
+                {
+                    tx = tx % ty;
+                }
+                else
+                {
+                    return((tx - sx) % ty == 0);
+                }
+            }
+            else
+            {
+                if(tx > sx)
+                {
+                    ty = ty % tx;
+                }
+                else
+                {
+                    return((ty - sy) % tx == 0);
+                }
+            }
+        }
+    
+           return (tx == sx && ty == sy);
+    
+    }
+};
