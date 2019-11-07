1315abcc3732cb16958cac6a8d009926f0258efd
Noopur R K <nrk25693@gmail.com>
2019-11-07 17:53:34 -0500

    Create findCeleb.cpp
    commit for : https://leetcode.com/problems/find-the-celebrity/

---
+++ findCeleb.cpp
@@ -0,0 +1,29 @@
+// Forward declaration of the knows API.
+bool knows(int a, int b);
+
+class Solution {
+public:
+    int findCelebrity(int n) 
+    {
+        int candidate = 0;
+        for(int i = 1; i < n; ++i)
+        {
+            if(knows(i,candidate) == false)
+            {
+                candidate = i;
+            }
+        }
+        
+        for(int i = 0 ; i < n; ++i)
+        {
+            if(i != candidate)
+            {
+                if(knows(i,candidate) == false || knows(candidate,i) == true)
+                {
+                    return -1;
+                }
+            }
+        }
+        return candidate;
+    }
+};
